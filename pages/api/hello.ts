d0a93f5d39e6df08a6d4005c34fe4fe6d2d8f7be
Rayan Castro <castrorry@iMac-de-Rayan.local>
2020-09-20 18:18:59 -0300

    remove hello api route

--- pages/api/hello.ts
+++
@@ -1,7 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { NowRequest, NowResponse } from '@vercel/node';
-
-export default (request: NowRequest, response: NowResponse) => {
-  response.statusCode = 200
-  response.json({ name: 'Rayan Castro' });
-}
